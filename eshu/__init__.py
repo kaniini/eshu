42eefe72e74a116ba5ee8788a8e9f67be716b47d
William Pitcock <nenolod@dereferenced.org>
2018-01-17 18:37:46 +0000

    base: integrate sanic_session module

--- eshu/__init__.py
+++ eshu/__init__.py
@@ -1,6 +1,9 @@
+import asyncio
+import aioredis
 from sanic import Sanic
 from gino.ext.sanic import Gino
 
+from sanic_session import RedisSessionInterface
 from sanic_jinja2 import SanicJinja2
 
 
@@ -13,6 +16,30 @@ db.init_app(app)
 
 
 jinja = SanicJinja2(app, pkg_name='eshu')
+
+
+class RedisPool:
+    _pool = None
+
+    async def get_redis_pool(self):
+        if not self._pool:
+            self._pool = await aioredis.create_pool(app.config.REDIS_ENDPOINT)
+
+        return self._pool
+
+
+redispool = RedisPool()
+session = RedisSessionInterface(redispool.get_redis_pool)
+
+
+@app.middleware('request')
+async def add_session_to_request(request):
+    await session_interface.open(request)
+
+
+@app.middleware('response')
+async def save_session(request, response):
+    await session_interface.save(request, response)
 
 
 from . import models
