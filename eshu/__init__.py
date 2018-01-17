6016e6222548fd91296976f94125ea521a0d46d6
William Pitcock <nenolod@dereferenced.org>
2018-01-17 05:27:20 +0000

    init: set up static dir (ugly hack for now), and templates (also ugly hack for now)

--- eshu/__init__.py
+++ eshu/__init__.py
@@ -1,12 +1,18 @@
 from sanic import Sanic
 from gino.ext.sanic import Gino
 
+from sanic_jinja2 import SanicJinja2
+
 
 app = Sanic()
 app.config.from_envvar('ESHU_CONFIG')
+app.static('/static', app.config.STATIC_DIR)
 
 db = Gino()
 db.init_app(app)
+
+
+jinja = SanicJinja2(app, pkg_name='eshu')
 
 
 from . import models
